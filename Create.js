7fec6945d26b0a8e09be78087f8b4145b3e1b3f8
Isabel <Isabel@1x-vl906-128-119-239-198.wireless.umass.edu>
2015-03-04 18:46:08 -0500

    update

--- Create.js
+++ Create.js
@@ -17,52 +17,6 @@ function newPassage(){
 function existingTitles(){
 	tarr.sort();
 	for(var i = 0; i < tarr.length; i++){
-		document.write(tarr[i]);
+		console.log(tarr[i]);
 	}
 }
-
-var D, form, bts, ipt;
-
-(function (W){
-	function init() {
-	    D = W.document, previous = [];
-	    form = D.getElementsByTagName('form')[0];
-	    bts = form.getElementsByTagName('button');
-	    ipt = form.getElementsByTagName('input');
-	    form.addEventListener('submit', save, false);
-	    bts[1].addEventListener('click', cancel, false);
-	    bts[2].addEventListener('click', edit, false);
-	}
-
-	function save(e) {
-	    e.preventDefault();
-	    form.classList.remove('invert');
-	    var l = ipt.length;
-	    while (l--) {
-	        ipt[l].readOnly = true;
-	    };
-	    previous = [];
-	    //send your info here 
-	}
-
-	function edit(e) {
-	    e.preventDefault();
-	    form.classList.add('invert');
-	    var l = ipt.length;
-	    while (l--) {
-	        previous[l] = ipt[l].value;
-	        ipt[l].readOnly = false;
-	    }
-	}
-
-	function cancel(e) {
-	    form.classList.remove('invert');
-	    e.preventDefault();
-	    var l = ipt.length;
-	    while (l--) {
-	        ipt[l].value = previous[l];
-	        ipt[l].readOnly = true;
-	    }
-	}
-	init();
-})(window)
