2eadedd2502877964b5c4ff16ca207c046a17621
Isabel <Isabel@1x-vl906-128-119-239-198.wireless.umass.edu>
2015-03-04 19:29:21 -0500

    update

--- Create.js
+++ Create.js
@@ -1,17 +1,51 @@
 var tarr = new Array();
+var parr = new Array();
+var larr = new Array();
 
 function newPassage(){
 	var title = prompt("Title Please");
 	var plot = prompt("Plot Please");
-	var linkTo = prompt("Link To Which Other Passages>");
+	var linkTo = prompt("Link To Which Other Passages");
 
 	tarr.push(title);
+	parr.push(plot);
+	larr.push(linkTo);
 
 }
 
 function existingTitles(){
 	tarr.sort();
 	for(var i = 0; i < tarr.length; i++){
-		window.alert(tarr[i]);
+		document.write(tarr[i]);
 	}
+}
+
+function display(){
+	for(var i = 0; i < tarr.length; i++){
+		document.write(tarr[i]);
+		document.write(parr[i]);
+		document.write(larr[i]);
+	}
+}
+
+function edit(){
+	var index = prompt("Which number would you like to edit?");
+	var option = prompt("Are you editing the Title, Prompt, or Links?");
+
+	if(option === "title" || option === "Title"){
+		var newT = prompt("New Title Please");
+		tarr[index] = newT;
+	}
+	if(option === "plot" || option === "Plot"){
+		var newP = prompt("New Plot Please");
+		parr[index] = newP;
+	}
+	if(option === "links" || option === "Links"){
+		var newL = prompt("New Links Please");
+		larr[index] = newL;
+	}
+}
+
+function play(){
+	
 }
